d74084c1f1d0dce5cff8ccd48c80b2231337c392
TimTom935 <timtom935school@gmail.com>
2024-10-29 16:50:39 -0600

    adds pc to project updates changes in alu

--- alu.v
+++ alu.v
@@ -4,44 +4,46 @@ module alu #(parameter WIDTH = 16)
              input      [2:0]       alucont, 
              output reg [WIDTH-1:0] result,
                  output reg [5:0] PSR // C F L Z N
-                 );
+                 );						  // 0 1 2 3 4 
 
-   wire     [WIDTH-1:0] b2, sum, slt;
+   wire [WIDTH-1:0] b2, sum, slt;
+	reg cary;
 
    always@(*) begin
       case(alucont)
-            3'b000: 
-            begin 
-                result <= Rsrc + Rdest; // Addition
-                PSR[0] = result < Rdest;
+            3'b000: // Addition
+            begin
+					 {cary, result} = Rsrc + Rdest;
+                PSR[0] = cary;
                 PSR[1] = (Rdest[WIDTH-1] == Rsrc[WIDTH-1]) && (result[WIDTH-1] != Rdest[WIDTH-1]);
             end
-            3'b001:
+            3'b001: // Subtraction
             begin
-                result <= Rsrc - Rdest; // Subtraction
-                PSR[0] <= (Rdest > Rsrc); // Set C flag if there's a borrow in subtraction.
-            PSR[1] <= (Rdest[WIDTH-1] != Rsrc[WIDTH-1]) && (result[WIDTH-1] != Rdest[WIDTH-1]); // Set F flag for signed overflow in subtraction.
-            PSR[2] <= (Rdest < Rsrc); // Set L flag if Rdest is less than Rsrc (unsigned comparison).
+               {cary, result} = Rsrc - Rdest;
+               PSR[0] = cary;
+					PSR[1] <= (Rdest[WIDTH-1] != Rsrc[WIDTH-1]) && (result[WIDTH-1] != Rdest[WIDTH-1]); // Set F flag for signed overflow in subtraction.
+					PSR[2] <= (Rdest < Rsrc); // Set L flag if Rdest is less than Rsrc (unsigned comparison).
          end
-            3'b010: 
+            3'b010: // Logical AND
             begin
-                result <= Rsrc & Rdest; // Logical AND
+               result <= Rsrc & Rdest; 
             end
-            3'b011: 
+            3'b011: // Logical XOR
             begin
-                result <= Rsrc ^ Rdest; // Logical XOR
+               result <= Rsrc ^ Rdest; 
             end
-            3'b100: 
+            3'b100: // Logical OR
             begin
-                result <= Rsrc | Rdest; // Logical OR
+               result <= Rsrc | Rdest; 
             end
+				3'b101: // Compare
+				begin
+					 // Set Zero flag (Z) if the result is zero.
+					result <= Rsrc - Rdest;
+					PSR[3] <= (Rsrc - Rdest == 0);
+					PSR[0] <= (Rdest < Rsrc);
+				end
             default: result <= 0; // Should never happen
       endcase
-
-        // Set Zero flag (Z) if the result is zero.
-      PSR[3] <= (result == 0);
-
-      // Set Negative flag (N) if the most significant bit (MSB) of the result is 1.
-      PSR[4] <= result[WIDTH-1]; 
     end
 endmodule
