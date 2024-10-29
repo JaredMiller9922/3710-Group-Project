a31a6fd6b8ae56d1b3b793664e4d56a58731b890
Jared Miller <jaredmschool9922@gmail.com>
2024-10-28 20:18:58 -0600

    adds starting datapath

--- Datapath.v
+++ Datapath.v
@@ -1,1 +1,15 @@
 // This is the datapath module
+module datapath #(paramter REGBITS = 3)
+(
+
+	// Create localparams
+	
+	// Create wires
+	
+	
+	// datapath registers and muxes 
+	
+	// Instantiate the register file and the alu
+);
+
+endmodule 
