5b70038563adf30b8b69b136dd5fd7eab90bcd13
Jared Miller <u1321655@utah.edu>
2024-11-12 15:22:29 -0700

    Updates the Datapath.drawio file as well as the verilog file to match

--- Datapath.v
+++ Datapath.v
@@ -1,10 +1,10 @@
 // This is the datapath module
 module datapath #(parameter WIDTH = 16, REGBITS = 3, IMM = 8, REG_ADD = 4)
 					  (input clk, reset,
-					   input wa_s, pc_s, alub_s, mem_s, // Selector bits for all mux2
-						input [1:0] wd_s, alua_s, 			// Selector bits for all mux4
+					   input pc_s, mem_s, 						// Selector bits for all mux2
+						input [1:0] wd_s, alua_s, alub_s,	// Selector bits for all mux4
 						input pcen,
-						input signext_sign,
+						input sign_ext_s,
 						input regwrite,
 						input [2:0] alucont,
 						input [IMM-1:0] imm,
@@ -43,14 +43,13 @@ module datapath #(parameter WIDTH = 16, REGBITS = 3, IMM = 8, REG_ADD = 4)
    flopr   #(WIDTH) rdest(clk, reset, rd2, Rdest);
 	
 	// datapath muxes all of the _s variables are control signals
-	mux2 #(WIDTH) wa_mux(Rsrc, Rdest, wa_s, wa);
-	mux2 #(WIDTH) mem_mux(Rsrc, pc_out, mem_s, mem_addr);
+	mux2 #(WIDTH) mem_mux(Rdest, pc_out, mem_s, mem_addr);
 	mux4 #(WIDTH) wd_mux(imm, Rsrc, mem_out, alu_out, wd_s, wd); // CONST_ZERO is a placeholder for no connection
 	mux2 #(WIDTH) pc_mux(Rsrc, alu_out, pc_s, pc_out);
 	mux4 #(WIDTH) alua_mux(Rsrc, pc_out, imm_ext, CONST_ZERO, alua_s, alua_out); // CONST_ZERO is a placeholder for no connection
-	mux2 #(WIDTH) alub_mux(Rdest, CONST_ONE, alub_s, alub_out);
+	mux4 #(WIDTH) alub_mux(Rdest, imm_ext, CONST_ONE, CONST_ONE, alub_s, alub_out);
 	
 	// Instantiate the register file and the alu
-   regfile    #(WIDTH,REGBITS) rf(clk, regwrite, rsrc_addr, rdest_addr, wa, wd, rd1, rd2);
+   regfile    #(WIDTH,REGBITS) rf(clk, regwrite, rsrc_addr, rdest_addr, Rdest, wd, rd1, rd2);
    alu        #(WIDTH) 			 alunit(Rsrc, Rdest, alucont, alu_out);
 endmodule 
