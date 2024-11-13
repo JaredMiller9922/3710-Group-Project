1fe439370afd5dc5c8920a5921406f6032d4eae4
Jared Miller <u1321655@utah.edu>
2024-11-13 10:52:02 -0700

    updates both drawio files and the datapath.v file to be uppercase

--- Datapath.v
+++ Datapath.v
@@ -1,18 +1,16 @@
 // This is the datapath module
-module datapath #(parameter WIDTH = 16, REGBITS = 3, IMM = 8, REG_ADD = 4, PSRL = 5)
+module datapath #(parameter WIDTH = 16, REGBITS = 3, IMML = 8, REG_ADD = 4, PSRL = 5)
 					  (input clk, reset,
-					   input pc_s, mem_s, 										// Selector bits for all mux2
-						input [1:0] wd_s, alua_s, alub_s,	   			// Selector bits for all mux4
-						input inst_en, alu_out_en, mem_reg_en, pc_en,	// Flopenr bits
-						input psr_en,												// Flopenr bits
+					   input PC_S, MEM_S, 										// Selector bits for all mux2
+						input [1:0] WD_S, ALUA_S, ALUB_S,	   			// Selector bits for all mux4
+						input INST_EN, ALU_OUT_EN, MEM_REG_EN, PC_EN,	// Flopenr bits
+						input PSR_EN,												// Flopenr bits
 						
-						input se_sign, reg_wr, 									// Control Signals
-						input [WIDTH-1:0] mem_out, instr,					// Inputs for mem_out and instr
+						input SE_SIGN, REG_WR, 									// Control Signals
+						input [WIDTH-1:0] MEM_OUT, INSTR,					// Inputs for MEM_OUT and INSTR
 						
-						input [REGBITS-1:0] alucont, 							// ALU control bits
-						
-						output [WIDTH-1:0] Rsrc, mem_addr,					// Values that allow memory access
-						output [PSRL-1:0] psr_out
+						output [WIDTH-1:0] Rsrc, MEM_ADDR,					// Values that allow memory access
+						output [PSRL-1:0] PSR_OUT
 );
 
 	// Create localparams
@@ -20,41 +18,41 @@ module datapath #(parameter WIDTH = 16, REGBITS = 3, IMM = 8, REG_ADD = 4, PSRL 
 	localparam CONST_ONE = 16'b0;
 	
 	// Create wires
-	wire [WIDTH-1:0] imm_ext, pc_out, pc, rd1, rd2, Rdest, alu_res, wd, alua_out, alub_out, mem_data_out, alu_out_val;
+	wire [WIDTH-1:0] IMM_EXT, PC_OUT, PC, Rdest, ALU_RES, WD, ALUA_OUT, ALUB_OUT, MEM_DATA_OUT, ALU_OUT_VAL;
 	
-	// Create wires for instruction decoding
-	wire[REG_ADD-1:0] rsrc_addr, rdest_addr, op_code, op_ext;
-	wire[IMM-1:0] imm;
+	// Create wires for INSTRuction decoding
+	wire[REG_ADD-1:0] Rsrc_addr, Rdest_addr, OP_CODE, OP_EXT;
+	wire[IMML-1:0] IMM;
 	
-	// Setting instruction fields
-	assign rsrc_addr = instr[3:0];
-	assign rdest_addr = instr[11:8];
-	assign imm = instr[7:0];
-	assign op_code = instr[15:12];
-	assign op_ext = instr[7:4]; 
+	// Setting INSTRuction fields
+	assign Rsrc_addr = INSTR[3:0];
+	assign Rdest_addr = INSTR[11:8];
+	assign IMM = INSTR[7:0];
+	assign OP_CODE = INSTR[15:12];
+	assign OP_EXT = INSTR[7:4]; 
 	
 	// create a sign extender
-	signextend #(IMM) extend(imm, se_sign, imm_ext);
+	signextend #(IMML) extend(IMM, SE_SIGN, IMM_EXT);
 	
 	// datapath registers
-	flopenr #(WIDTH) pcreg(clk, reset, pc_en, pc_out, pc);
-	flopenr #(WIDTH) instr_reg(clk, reset, inst_en, mem_out, instr);
-	flopenr #(WIDTH) alu_out(clk, reset, alu_out_en, alu_res, alu_out_val);
-	flopenr #(WIDTH) mem_data_reg(clk, reset, mem_reg_en, mem_out, mem_data_out);
-	flopenr #(WIDTH) psr_reg(clk, reset, psr_en, PSR, psr_out);
+	flopenr #(WIDTH) pcreg(clk, reset, PC_EN, PC_OUT, PC);
+	flopenr #(WIDTH) instr_reg(clk, reset, INST_EN, MEM_OUT, INSTR);
+	flopenr #(WIDTH) alu_out(clk, reset, ALU_OUT_EN, ALU_RES, ALU_OUT_VAL);
+	flopenr #(WIDTH) mem_data_reg(clk, reset, MEM_REG_EN, MEM_OUT, MEM_DATA_OUT);
+	flopenr #(WIDTH) psr_reg(clk, reset, PSR_EN, PSR, PSR_OUT);
 	flopr   #(WIDTH) rsrc(clk, reset, rd1, Rsrc);	
    flopr   #(WIDTH) rdest(clk, reset, rd2, Rdest);
 	
 	// datapath muxes all of the _s variables are control signals
-	mux2 #(WIDTH) mem_mux(Rdest, pc_out, mem_s, mem_addr);
-	mux2 #(WIDTH) pc_mux(Rsrc, alu_out, pc_s, pc_out);
-	mux4 #(WIDTH) wd_mux(imm_ext, Rsrc, mem_out, alu_out, wd_s, wd); // CONST_ZERO is a placeholder for no connection
-	mux4 #(WIDTH) alua_mux(Rsrc, pc_out, imm_ext, CONST_ZERO, alua_s, alua_out); // CONST_ZERO is a placeholder for no connection
-	mux4 #(WIDTH) alub_mux(Rdest, imm_ext, CONST_ONE, CONST_ONE, alub_s, alub_out);
+	mux2 #(WIDTH) mem_mux(Rdest, PC_OUT, MEM_S, MEM_ADDR);
+	mux2 #(WIDTH) pc_mux(Rsrc, alu_out, PC_S, PC_OUT);
+	mux4 #(WIDTH) wd_mux(IMM_EXT, Rsrc, MEM_OUT, alu_out, WD_S, WD); // CONST_ZERO is a placeholder for no connection
+	mux4 #(WIDTH) alua_mux(Rsrc, PC_OUT, IMM_EXT, CONST_ZERO, ALUA_S, ALUA_OUT); // CONST_ZERO is a placeholder for no connection
+	mux4 #(WIDTH) alub_mux(Rdest, IMM_EXT, CONST_ONE, CONST_ONE, ALUB_S, ALUB_OUT);
 	
 	// Instantiate the register file, the alu, the alucont
-   regfile    #(WIDTH,REGBITS) rf(clk, reg_wr, rsrc_addr, rdest_addr, Rdest, wd, rd1, rd2);
-   alu        #(WIDTH) 			 alunit(Rsrc, Rdest, alucont, alu_res, PSR);
-	alucontrol alu_cont(op_code, op_ext, alucont);
+   regfile    #(WIDTH,REGBITS) rf(clk, REG_WR, Rsrc_addr, Rdest_addr, Rdest, WD, rd1, rd2);
+   alu        #(WIDTH) 			 alunit(Rsrc, Rdest, alucont, ALU_RES, PSR);
+	alucontrol alu_cont(OP_CODE, OP_EXT, alucont);
 						
 endmodule 
